651cab7a6cf2d9187eea14b061df5961aaed8efc
Mario Gayo <mgayo@astrolabio.net>
2015-06-03 00:55:02 +0200

    Vistas parciales y marco de la aplicación
    Se ha instalado el paquete express-partials y se han modificado las
    vistas para generar las páginas de manera estructurada por partes.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// Incluimos un paquete para permitir páginas por 'partes'
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 
@@ -12,6 +14,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Generamos el objeto para usar las vistas parciales
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
