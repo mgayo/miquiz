df4169e489a716a0a4d2cd95b8bd18451dc412a2
Mario Gayo <mgayo@astrolabio.net>
2015-06-02 20:42:31 +0200

    Primera pregunta
    Se ha añadido un primer controlador y dos nuevas vistas (question y
    answer) para gestionar las preguntas-respuestas y se ha incluido una
    pregunta de prueba.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+// Importamos el controlador básico
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'miQuiz' });
 });
+
+// Enlazamos las rutas con el controlador que debe gestionarlas
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer',quizController.answer);
 
 module.exports = router;
